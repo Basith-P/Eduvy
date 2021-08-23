18e5e80f90e14842371190aa11ec43231c159b48
Basith P <basithpofficial@gmail.com>
2021-08-23 20:33:18 +0530

    add listview

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -6,121 +6,162 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       backgroundColor: Color(0xfff5f5f5),
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 20),
-          child: Column(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 20),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Hello,',
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text(
+                          'Esther Howard',
+                          style: TextStyle(
+                            fontSize: 26,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                    IconButton(
+                      onPressed: () {},
+                      icon: Icon(Icons.menu_rounded),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 20),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 10),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
                       Text(
-                        'Hello,',
+                        'Popular courses',
                         style: TextStyle(
-                          fontSize: 20,
                           fontWeight: FontWeight.bold,
+                          fontSize: 20,
                         ),
                       ),
-                      Text(
-                        'Esther Howard',
-                        style: TextStyle(
-                          fontSize: 26,
-                          fontWeight: FontWeight.bold,
-                        ),
+                      TextButton(
+                        onPressed: () {},
+                        child: Text('view all'),
                       ),
                     ],
                   ),
-                  IconButton(
-                    onPressed: () {},
-                    icon: Icon(Icons.menu_rounded),
-                  ),
-                ],
-              ),
-              const SizedBox(height: 20),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    'Popular courses',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20,
-                    ),
-                  ),
-                  TextButton(
-                    onPressed: () {},
-                    child: Text('view all'),
-                  ),
-                ],
-              ),
-              Row(
-                children: [
-                  Expanded(
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(20),
-                      child: Column(
-                        children: [
-                          Image.asset('assets/images/graphic-design.jpg'),
-                          Container(
-                            color: Colors.white,
-                            padding: EdgeInsets.all(15),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                const SizedBox(height: 10),
-                                Text(
-                                  'Graphic Design',
-                                  style: TextStyle(
-                                      fontWeight: FontWeight.bold,
-                                      fontSize: 20),
+                ),
+                Container(
+                  height: 250,
+                  child: ListView(
+                    scrollDirection: Axis.horizontal,
+                    children: [
+                      Container(
+                        width: 200,
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(20),
+                          child: Column(
+                            children: [
+                              Container(
+                                height: 120,
+                                child: Image.asset(
+                                  'assets/images/graphic-design.jpeg',
+                                  fit: BoxFit.cover,
                                 ),
-                                const SizedBox(height: 10),
-                                Text('Learn the basics of the design course'),
-                                const SizedBox(height: 10),
-                                Text('- 3 h 15 min'),
-                              ],
-                            ),
-                          )
-                        ],
+                              ),
+                              Container(
+                                color: Colors.white,
+                                padding: EdgeInsets.all(15),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      'Graphic Design',
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 18,
+                                      ),
+                                    ),
+                                    const SizedBox(height: 10),
+                                    Text(
+                                        'Learn the basics of the design course'),
+                                    const SizedBox(height: 10),
+                                    Text('- 3 h 15 min'),
+                                  ],
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                      const SizedBox(width: 15),
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(20),
+                        child: Container(
+                          width: 200,
+                          child: Column(
+                            children: [
+                              Container(
+                                height: 120,
+                                child: Image.asset(
+                                  'assets/images/digital-marketing.jpg',
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                              Container(
+                                color: Colors.white,
+                                padding: EdgeInsets.all(15),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      'Digital Marketing',
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 18),
+                                    ),
+                                    const SizedBox(height: 10),
+                                    Text(
+                                        'Learn the basics of digital marketing'),
+                                    const SizedBox(height: 10),
+                                    Text('- 3 h 15 min'),
+                                  ],
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 20),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Our Top Mentors',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
                       ),
                     ),
-                  ),
-                  const SizedBox(width: 15),
-                  Expanded(
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(20),
-                      child: Column(
-                        children: [
-                          Image.asset('assets/images/graphic-design.jpg'),
-                          Container(
-                            color: Colors.white,
-                            padding: EdgeInsets.all(15),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                const SizedBox(height: 10),
-                                Text(
-                                  'Graphic Design',
-                                  style: TextStyle(
-                                      fontWeight: FontWeight.bold,
-                                      fontSize: 20),
-                                ),
-                                const SizedBox(height: 10),
-                                Text('Learn the basics of the design course'),
-                                const SizedBox(height: 10),
-                                Text('- 3 h 15 min'),
-                              ],
-                            ),
-                          )
-                        ],
-                      ),
+                    TextButton(
+                      onPressed: () {},
+                      child: Text('view all'),
                     ),
-                  )
-                ],
-              )
-            ],
+                  ],
+                )
+              ],
+            ),
           ),
         ),
       ),
